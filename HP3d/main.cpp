8071469d70af37b0806189504b6f8ffaf3b49513
Marcin Skotniczny <marcin@swmansion.com>
2015-06-05 19:04:26 +0200

    Initial Commit

---
+++ HP3d/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HP3d
+//
+//  Created by Marcin on 05.06.2015.
+//  Copyright (c) 2015 Marcin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
